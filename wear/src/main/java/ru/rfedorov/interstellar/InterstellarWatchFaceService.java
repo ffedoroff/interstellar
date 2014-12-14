b8dcca104700dd3d9f89719c19e8149c4850a3a9
Roman Fedorov <rfedorov@linkentools.com>
2014-12-14 13:21:54 +0600

    add watch listener

--- wear/src/main/java/ru/rfedorov/interstellar/InterstellarWatchFaceService.java
+++ wear/src/main/java/ru/rfedorov/interstellar/InterstellarWatchFaceService.java
@@ -21,6 +21,13 @@ import android.text.format.Time;
 import android.util.Log;
 import android.view.SurfaceHolder;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.MessageApi;
+import com.google.android.gms.wearable.Node;
+import com.google.android.gms.wearable.NodeApi;
+import com.google.android.gms.wearable.Wearable;
+
 import java.util.TimeZone;
 import java.util.concurrent.TimeUnit;
 
@@ -31,14 +38,89 @@ import java.util.concurrent.TimeUnit;
  *
  *
  */
-public class InterstellarWatchFaceService extends CanvasWatchFaceService {
+public class InterstellarWatchFaceService extends CanvasWatchFaceService implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
     private static final String TAG = "AnalogWatchFaceService";
+    private static final String msgPathMobile = "/rfedorov_mobile";
+    GoogleApiClient googleClient;
 
     /**
      * Update rate in milliseconds for interactive mode. We update once a second to advance the
      * second hand.
      */
     private static final long INTERACTIVE_UPDATE_RATE_MS = TimeUnit.SECONDS.toMillis(1);
+
+    private void sendInitMessage() {
+        Log.v(TAG, "sendInitMessage");
+        new SendToDataLayerThread("init").start();
+    }
+
+    public class MessageReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String msg = intent.getStringExtra("message");
+            String[] data = msg.split(",");
+            if (data.length > 1) {
+                if ("set-values".equals(data[0])) {
+//                    for (int i=1; i<Math.min(data.length, buttons.length+1); i++) {
+//                        buttons[i-1].setText(data[i]);
+//                        buttons[i-1].setBackgroundResource(R.drawable.bulb_off);
+//                        buttons[i-1].setTag(false);
+//                    }
+                }
+            }
+        }
+    }
+
+    // Send a message when the data layer connection is successful.
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        Log.v(TAG, "onConnected");
+        sendInitMessage();
+    }
+
+    // Disconnect from the data layer when the Activity stops
+//    @Override
+    protected void onStop() {
+        if (null != googleClient && googleClient.isConnected()) {
+            googleClient.disconnect();
+        }
+//        super.onStop();
+    }
+
+    // Placeholders for required connection callbacks
+    @Override
+    public void onConnectionSuspended(int cause) {
+        Log.e(TAG, "wear onConnectionSuspended cause:"+cause);
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.e(TAG, "wear onConnectionFailed result:"+connectionResult);
+    }
+
+    class SendToDataLayerThread extends Thread {
+        String message;
+
+        // Constructor to send a message to the data layer
+        SendToDataLayerThread(String msg) {
+            message = msg;
+        }
+
+        public void run() {
+            NodeApi.GetConnectedNodesResult nodes = Wearable.NodeApi.getConnectedNodes(googleClient).await();
+            for (Node node : nodes.getNodes()) {
+                MessageApi.SendMessageResult result = Wearable.MessageApi.sendMessage(googleClient, node.getId(), msgPathMobile, message.getBytes()).await();
+                if (result.getStatus().isSuccess()) {
+                    Log.v(TAG, "Message: {" + message + "} sent to phone: " + node.getDisplayName());
+                }
+                else {
+                    // Log an error
+                    Log.e(TAG, "ERROR: failed to send Message to phone: {" + message + "} watch: " + node.getDisplayName());
+                }
+            }
+        }
+    }
 
     @Override
     public Engine onCreateEngine() {
